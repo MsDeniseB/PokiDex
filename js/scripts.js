9f8050dcb21b5d05885d37dce51e7a9935147f86
MsDeniseB <Denisebrazier2019@gmail.com>
2021-10-21 02:42:33 -0700

    Updated website layout
    If committed this will add a navigation bar, logo, favicon, and new pages for the website.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-alert('Hello world');
-
-let favoritefood = 'apple'
-document.write(favoritefood); 
-
-
+const pokemonList = [
+    { name: 'eevee', height: 1, type: 'normal'},
+    { name: 'weedle', height: 1, type: 'bug'},
+    { name: 'ekans', height: 6, type: 'poison'}
+  ];
 
