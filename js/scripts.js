fd4c0976480e67d2e0939c2be1b21ecffabc52ba
MsDeniseB <Denisebrazier2019@gmail.com>
2021-10-25 15:28:36 -0700

    1.3 task update
    If committed  the updated code would  take the Pokémon array and display it using a four loop. also updated appearance on all pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 const pokemonList = [
-    { name: 'eevee', height: 1, type: 'normal'},
-    { name: 'weedle', height: 1, type: 'bug'},
-    { name: 'ekans', height: 6, type: 'poison'}
+    { name: 'eevee', height: 0.7, type: 'normal'},
+    { name: 'weedle', height: 1.7, type: 'bug'},
+    { name: 'ekans', height: 1, type: 'poison'}
   ];
 
+pokemonList.forEach(function(pokemon) {
+if(pokemon.height <= 1) {
+  document.write("name" + pokemon.name + " ("+pokemon.height+")")
+  document.write("<br>")
+}
+else{
+  document.write("name" + pokemon.name + " ("+pokemon.height+")" + "Wow that's big!")
+  document.write("<br>")
+}
+})
+
+const formLabels = ["fullname", "username", "password", "address", "city"];
+
+const doc = document.getElementById("root");
+doc.innerHTML+= "<form action='www.google.com' method='get'>"
+formLabels.forEach(function(label, index){
+doc.innerHTML+= label+": <input type='text'>"+"<br>";
+});
+doc.innerHTML+= "<button>Submit</button>"
+doc.innerHTML+= "</form>"
+
+
+document.write('hello there')
