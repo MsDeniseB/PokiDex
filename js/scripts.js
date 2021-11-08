05c1b8f3ca0f38824c25bcd233f76a7ce6cba8bf
MsDeniseB <Denisebrazier2019@gmail.com>
2021-11-08 06:22:58 -0800

    Task 1.5
    If this commit is applied It will update java code to be in IIFE and display Pokémon in array

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,50 @@ const pokemonList = [
     { name: 'ekans', height: 1, type: 'poison'}
   ];
 
-pokemonList.forEach(function(pokemon) {
-if(pokemon.height <= 1) {
-  document.write("name" + pokemon.name + " ("+pokemon.height+")")
-  document.write("<br>")
-}
-else{
-  document.write("name" + pokemon.name + " ("+pokemon.height+")" + "Wow that's big!")
-  document.write("<br>")
-}
-})
+  //forEach function
 
+  pokemonList.forEach(function(pokemon) {
+    if(pokemon.height <= 1) {
+      document.write("name" + pokemon.name + " ("+pokemon.height+")")
+      document.write("<br>")
+    }
+    else{
+      document.write("name" + pokemon.name + " ("+pokemon.height+")" + "Wow that's big!")
+      document.write("<br>")
+    }
+    })
+
+  let pokemonRepositoy = (function(){
+    let pokemonList = [
+      { name: 'eevee', height: 0.7, type: 'normal'},
+      { name: 'weedle', height: 1.7, type: 'bug'},
+      { name: 'ekans', height: 1, type: 'poison'}
+    ];
+   
+    return {
+      add : function (pokemon){
+        pokemonList.push(pokemon);
+        },
+   
+       function getAll() {
+          return pokemonList;
+        }
+     
+        return {
+          add: add,
+          getAll: getAll 
+        };
+      })();
+
+ 
+
+   
+
+  
+
+
+
+/*
 const formLabels = ["fullname", "username", "password", "address", "city"];
 
 const doc = document.getElementById("root");
@@ -24,6 +57,5 @@ doc.innerHTML+= label+": <input type='text'>"+"<br>";
 });
 doc.innerHTML+= "<button>Submit</button>"
 doc.innerHTML+= "</form>"
+*/
 
-
-document.write('hello there')
