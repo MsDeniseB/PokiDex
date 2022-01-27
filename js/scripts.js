31ae4b7e01edb7494ea3a12c84ce35efe10ad39e
MsDeniseB <Denisebrazier2019@gmail.com>
2022-01-27 05:37:32 -0800

    1.5 Update
    With this commit the page will display 3 Pokémon and comments on the largest sized Pokémon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 const pokemonList = [
   { name: 'eevee', height: 0.7, type: 'normal'},
   { name: 'weedle', height: 1.7, type: 'bug'},
@@ -38,9 +40,11 @@ let pokemonRepositoy = (function(){
     })();
 
 
+   
 
-/* form for name and info
 
+
+/*
 const formLabels = ["fullname", "username", "password", "address", "city"];
 
 const doc = document.getElementById("root");
@@ -50,37 +54,5 @@ doc.innerHTML+= label+": <input type='text'>"+"<br>";
 });
 doc.innerHTML+= "<button>Submit</button>"
 doc.innerHTML+= "</form>"
-
-
-
-  function add(pokemon) {
-    if(
-      typeof pokemon === "objecet" &&
-      "name" in pokemon &&
-      "height" in pokemon &&
-      "types" in pokemon
-      ) {
-      repository.push(pokemon);
-      } else {
-        console.log("pokemon is not correct");
-
-
-
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({name: 'eevee', height: 0.7, type: 'normal'});
-
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.getAll().forEach(fuction (pokemon) {
-  let pokemonList = document.querySelector(".pokemon-list");
-  let listpokemon = document.createElement("li");
-  let button = document.createElement("buttton");
-  button.innerText = pokemon.name;
-  button.classList.add("button-class");
-  listpokemon.appendChild(button);
-  pokemonList.appendChild(listpokemon);
-
-});
-
 */
 
