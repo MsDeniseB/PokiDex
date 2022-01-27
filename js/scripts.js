50f9826fa4229b5c4e987b407ee7ba541a4bd722
MsDeniseB <Denisebrazier2019@gmail.com>
2022-01-27 07:38:43 -0800

    1.6 (Done)
    With this commit the home screen displays three Pokémon buttons that when interacted with show the type and height in a pop-up format.

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,60 @@
 const pokemonList = [
-    { name: 'eevee', height: 0.7, type: 'normal'},
-    { name: 'weedle', height: 1.7, type: 'bug'},
-    { name: 'ekans', height: 1, type: 'poison'}
-  ];
-
-  //forEach function
-
-  pokemonList.forEach(function(pokemon) {
-    if(pokemon.height <= 1) {
-      document.write("name" + pokemon.name + " ("+pokemon.height+")")
-      document.write("<br>")
-    }
-    else{
-      document.write("name" + pokemon.name + " ("+pokemon.height+")" + "Wow that's big!")
-      document.write("<br>")
-    }
-    })
-
-  let pokemonRepositoy = (function(){
-    let pokemonList = [
-      { name: 'eevee', height: 0.7, type: 'normal'},
-      { name: 'weedle', height: 1.7, type: 'bug'},
-      { name: 'ekans', height: 1, type: 'poison'}
-    ];
-   
-    return {
-      add : function (pokemon){
-        pokemonList.push(pokemon);
-        },
-   
-       function getAll() {
-          return pokemonList;
-        }
-     
-        return {
-          add: add,
-          getAll: getAll 
-        };
-      })();
-
+  { name: 'eevee', height: 0.7, types: 'normal'},
+  { name: 'weedle', height: 1.7, types: 'bug'},
+  { name: 'ekans', height: 1, types: 'poison'}
+];
  
-
-   
-
-  
-
-
-
-/*
+//forEach function
+ 
+  // functions
+let pokemonRepositoy = (function(){
+ 
+   function add (pokemon){
+      pokemonList.push(pokemon);
+    };
+ 
+   function getAll() {
+        return pokemonList;
+    };
+ 
+    function addListItem(pokemon){
+      let pokemonList = document.querySelector(".pokemon-list");
+      console.log(pokemonList)
+      let listpokemon = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("button-class");
+      button.onclick = (()=>{
+      showdetails(pokemon)
+      })
+      listpokemon.appendChild(button);
+      pokemonList.appendChild(listpokemon);
+ 
+ 
+    function showdetails(pokemon){
+      alert(`Name: ${pokemon.name} \n
+      Height: ${pokemon.height}
+      Type: ${pokemon.types}`)
+    }
+    };
+      //function return add a return for each functions
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem
+    };
+    })();
+ 
+    pokemonRepositoy.getAll().forEach(function (pokemon){
+      pokemonRepositoy.addListItem(pokemon);
+    });
+ 
+ 
+ 
+/* form for name and info
+ 
 const formLabels = ["fullname", "username", "password", "address", "city"];
-
+ 
 const doc = document.getElementById("root");
 doc.innerHTML+= "<form action='www.google.com' method='get'>"
 formLabels.forEach(function(label, index){
@@ -57,5 +62,39 @@ doc.innerHTML+= label+": <input type='text'>"+"<br>";
 });
 doc.innerHTML+= "<button>Submit</button>"
 doc.innerHTML+= "</form>"
+ 
+ 
+ 
+  function add(pokemon) {
+    if(
+      typeof pokemon === "objecet" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+      ) {
+      repository.push(pokemon);
+      } else {
+        console.log("pokemon is not correct");
+ 
+ 
+ 
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({name: 'eevee', height: 0.7, type: 'normal'});
+ 
+console.log(pokemonRepository.getAll());
+ 
+pokemonRepository.getAll().forEach(fuction (pokemon) {
+  let pokemonList = document.querySelector(".pokemon-list");
+  let listpokemon = document.createElement("li");
+  let button = document.createElement("buttton");
+  button.innerText = pokemon.name;
+  button.classList.add("button-class");
+  listpokemon.appendChild(button);
+  pokemonList.appendChild(listpokemon);
+ 
+});
+ 
 */
+ 
+ 
 
