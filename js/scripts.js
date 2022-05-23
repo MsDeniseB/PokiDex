1109604cc734bf9e9c574bedb478c61f49a1d9fc
MsDeniseB <Denisebrazier2019@gmail.com>
2022-05-23 16:27:35 -0700

    1.8 Task
    Update for the name and height to appear

--- js/scripts.js
+++ js/scripts.js
@@ -32,9 +32,15 @@ let pokemonRepositoy = (function(){
     }
 
     function showdetails(pokemon){
+      console.log(pokemon)
+     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+     .then(res=>res.json())
+     .then(pokemon=>{
       alert(`Name: ${pokemon.name} \n
       Height: ${pokemon.height}
-      Type: ${pokemon.types}`)
+      Type: ${pokemon.types.map(x=>x.type.name)}`)
+     })
+     
     }
     
 
@@ -57,6 +63,74 @@ let pokemonRepositoy = (function(){
 
     }
  
+    function validateEmail() {
+        let value = emailInput.value;
+      
+        if (!value) {
+          showErrorMessage(emailInput, 'Email is a required field.');
+          return false;
+        }
+      
+        if (value.indexOf('@') === -1) {
+          showErrorMessage(emailInput, 'You must enter a valid email address.');
+          return false;
+        }
+      
+        showErrorMessage(emailInput, null);
+        return true;
+      }
+    
+      function validatePassword() {
+        let value = passwordInput.value;
+      
+        if (!value) {
+          showErrorMessage(passwordInput, 'Password is a required field.');
+          return false;
+        }
+      
+        if (value.length < 8) {
+          showErrorMessage(passwordInput, 'The password needs to be at least 8 characters long.');
+          return false;
+        }
+      
+        showErrorMessage(passwordInput, null);
+        return true;
+      }
+
+    function showErrorMessage(input, message) {
+      let container = input.parentElement;
+      //Removes an existing error
+      let error = container.querySelector('.error-message');
+      if (error) {
+        container.removeChild(error);
+      }
+      // Now add the error, if the message is not empty
+      if (message) {
+        let error = document.createElement('div');
+        error.classList.add('error-message');
+        error.innerText = message;
+        container.appendChild(error);
+      }
+    }
+
+
+   function validateForm() {
+      let isValidEmail = validateEmail();
+      let isValidPassword = validatePassword();
+      return isValidEmail && isValidPassword;
+    }
+        //eror here 4 sum reson
+    /* 
+    form.addEventListener('submit', (e) => {
+      e.preventDefault(); // Do not submit to the server
+      if (validateForm()) {
+        alert('Success!');
+      }
+    });
+    
+    emailInput.addEventListener('input', validateEmail);
+    passwordInput.addEventListener('input', validatePassword); */
+
     function main() {
       pokemonRepositoy.loadList("https://pokeapi.co/api/v2/pokemon/");
     }
@@ -67,7 +141,11 @@ let pokemonRepositoy = (function(){
         addListItem: addListItem,
         loadList: loadList, 
         showdetails: showdetails,
-        main: main
+        main: main,
+        validateEmail: validateEmail,
+        validatePassword: validatePassword,
+        showErrorMessage: showErrorMessage,
+        validateForm: validateForm
     };
 
 
